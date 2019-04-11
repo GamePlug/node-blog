87c2445fb661ec5e71e47d1d5c93089974f06c38
LeiChao <540007827@qq.com>
2019-04-12 01:44:49 +0800

    Add iview

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,9 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [],
+  plugins: [
+    {src: '@plugins/iview', ssr: true}
+  ],
 
   /*
   ** Nuxt.js modules
